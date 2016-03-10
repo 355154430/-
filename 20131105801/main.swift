676fe72b8bfd482cb19c245b6a1e7fcc0453096e
355154430 <355154430@qq.com>
2016-03-10 10:27:26 +0800

    1.3
    1.3

--- 20131105801/main.swift
+++ 20131105801/main.swift
@@ -19,3 +19,24 @@ print("Hello, World!!")
 //  Copyright © 2016年 King. All rights reserved.
 //
 
+
+///////////////////////////////////////////////////////
+
+/*import Cocoa
+func hello(num:Int)
+{
+print("hello\(num)")
+}
+for var i in 1...20
+{
+hello(i)
+}*/
+
+
+
+/*import Cocoa
+func add(a x:Int,b y:Int,c z:Int)
+{
+print("\(x)+\(y)+\(z)=\(x+y+z)")
+}
+add(a:10,b:20,c:30)*/
